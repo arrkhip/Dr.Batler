4c635f5a3d8ffe6af324aa814a8dbd6f83c74109
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2019-09-25 13:46:33 +0300

    created about section

--- scripts/main.js
+++ scripts/main.js
@@ -5,47 +5,13 @@ $('.js-header__switch').click(function() {
 	$('body').toggleClass('scroll-hidden');
 });
 
-
-//header example 
-$('.js-header__authorization').click(function() {
-	$('.header__authorization-inner').slideToggle(300);
+// slider 
+var swiper = new Swiper('.about__slider', {
+  slidesPerView: 1,
+  loop: true,
+  speed: 500,
+  pagination: {
+    el: '.about__slider-pagination',
+    clickable: true,
+  },
 });
-
-$('.js-header__cart').click(function() {
-	$('.header__cart-inner').slideToggle(300);
-});
-
-$('.js-header__searche').click(function() {
-	$('.header__searche-inner').slideToggle(300);
-});
-
-$('.js-header-btn').click(function() {
-  $(this).not(this).parent().find('.js-header-btn').removeClass('active');
-  if($(this).hasClass('active')){
-    $(this).removeClass('active');
-    }else{
-      $(this).addClass('active');
-    }
-});
-
-
-
-// video player
-var overlay = document.getElementById('overlay');
-var vid = document.getElementById('video');
-
-if(overlay.addEventListener){
-		overlay.addEventListener("click", play, false)
-	}else if(overlay.attachEvent){
-		overlay.attachEvent("onclick", play)
-	}
-
-function play() { 
-    if (vid.paused){
-        vid.play(); 
-        overlay.classList.add("test");
-    }else {
-        vid.pause(); 
-        overlay.classList.remove("test");
-    }
-} 
